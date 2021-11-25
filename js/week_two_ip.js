495f7d5af0999ae94b502d0c015cb2925d8a9632
Eunice Otieno <otieunice@gmail.com>
2021-11-25 11:05:30 +0300

    Modified the function

--- js/week_two_ip.js
+++ js/week_two_ip.js
@@ -6,15 +6,15 @@ MM -  is the Month
 DD - is the Day of the month 
 mod - is the modulus function ( % )*/
 
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames = ["Kwame","Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+var femaleNames = ["Ama","Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
 
 //Calculate yy values
 function calculateYear(dob) {
 
     var dobTostring = dob.toString().split('').slice(2, 4);
     var yy = dobTostring.toString().replace(",", "");
-    yy=parseInt(yy);
+    yy = parseInt(yy);
     return yy;
 }
 //calculate century digits
@@ -26,54 +26,54 @@ function calculateCenturyDigits(dob) {
 }
 
 //Calculate day of the week value
-function calculateDay(dob, mm,dd) {
+function calculateDay(dob, mm, dd) {
     var yy = calculateYear(dob.value);
-    var cc = calculateCenturyDigits(dob.value); 
-    console.log("YY"+yy+"CC "+cc+" MM"+ mm.value + "DD"+dd.value);
-    c = parseInt((cc/4) - 2*cc-1);
+    var cc = calculateCenturyDigits(dob.value);
+    console.log("YY" + yy + "CC " + cc + " MM" + mm.value + "DD" + dd.value);
+    c = parseInt((cc / 4) - 2 * cc - 1);
     console.log(c);
-    y = parseInt(5*yy/4);
+    y = parseInt(5 * yy / 4);
     console.log(y);
-    m = parseInt(26*(mm.value+1)/10);
+    m = parseInt(26 * (mm.value + 1) / 10);
     console.log(m);
     d = parseInt(dd.value);
     console.log(d);
-    var h = c+y+m+d;
-    var dayofweek = (c+y+m+d) %7;
-    
-    console.log("mod "+h);
-    var day= parseInt(dayofweek);
-    console.log(dayofweek);
+    var h = c + y + m + d;
+    var dayofweek = (c + y + m + d) % 7;
+
+    console.log("mod " + h);
+    var day = parseInt(dayofweek);
+    console.log("day: "+day);
 
     return day;
 
 }
 // Return the Akan name
-function calculateName(dob,mm,gender,day) {
-    if(dob.value == "" || mm.value == "" || gender.value== "" || day.value== ""){
+function calculateName(dob, mm, gender, day) {
+    if (dob.value == "" || mm.value == "" || gender.value == "" || day.value == "") {
         return alert("Enter the required details");
     }
-    if(mm.value<=0 || mm.value > 12){
+    if (mm.value <= 0 || mm.value > 12) {
         return alert("Enter month value between 1 and 12");
     }
-    if(day.value <= 0 || day.value >31){
+    if (day.value <= 0 || day.value > 31) {
         return alert("Enter day value between 1 and 31")
     }
-    var d = calculateDay(dob,mm,day);
-    console.log("d"+ d);
-    console.log("d"+ gender.value);
-    var name="";
-    if(gender.value == "male"){
+    var d = calculateDay(dob, mm, day);
+    console.log("d" + d);
+    console.log("d" + gender.value);
+    var name = "";
+    if (gender.value == "male") {
         name = maleNames[d];
     }
-    else if(gender.value ="female"){
-        console.log("here:" +femaleNames[d]);
+    else if (gender.value = "female") {
+        console.log("here:" + femaleNames[d]);
         name = femaleNames[d];
     }
-    else{
-        name ="Not Found";
+    else {
+        name = "Not Found";
     }
-    alert("Your Akan name is "+name);
+    alert("Your Akan name is " + name);
     console.log(name);
     return name;
 }
